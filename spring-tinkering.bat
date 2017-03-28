43c0bc3b94c5792f31a93427896f4f98245d33d4
sleyzerzon <senya22@yahoo.com>
2017-03-28 07:02:59 -0400

    lombok is collocated

--- spring-tinkering.bat
+++ spring-tinkering.bat
@@ -1,1 +1,3 @@
-start /B C:\spring-tool-suite-3.6.4.RELEASE-e4.4.2-win32-x86_64\sts-3.6.4.RELEASE\STS.exe -data "E:\source\spring-tinkering" -clean -showlocation -vmC:\Java\java-8 -vmargs -Xmx1024m -XX:MaxPermSize=256m -javaagent:lombok.jar -Xbootclasspath/a:E:\source\spring-tinkering\lombok.jar
+rem start /B C:\spring-tool-suite-3.6.4.RELEASE-e4.4.2-win32-x86_64\sts-3.6.4.RELEASE\STS.exe -data "E:\source\spring-tinkering" -clean -showlocation -vmC:\Java\java-8 -vmargs -Xmx1024m -XX:MaxPermSize=256m -javaagent:lombok.jar -Xbootclasspath/a:E:\source\spring-tinkering\lombok.jar
+
+start /B C:\spring-tool-suite-3.6.4.RELEASE-e4.4.2-win32-x86_64\sts-3.6.4.RELEASE\STS.exe -data "E:\source\spring-tinkering" -clean -showlocation -vmC:\Java\java-8 -vmargs -Xmx1024m -XX:MaxPermSize=256m -javaagent:lombok.jar -Xbootclasspath/a:lombok.jar
