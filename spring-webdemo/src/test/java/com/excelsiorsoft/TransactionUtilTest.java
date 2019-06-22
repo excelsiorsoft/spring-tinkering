97117c836cfaeb72e95969dcfcedd568dc0080e1
unknown <>
2019-06-22 19:52:53 -0400

    add tests for work

--- spring-webdemo/src/test/java/com/excelsiorsoft/TransactionUtilTest.java
+++ spring-webdemo/src/test/java/com/excelsiorsoft/TransactionUtilTest.java
@@ -21,12 +21,17 @@ public class TransactionUtilTest {
 
 public void testDoubleFormat() {
 //https://stackoverflow.com/questions/8819842/best-way-to-format-a-double-value-to-2-decimal-places
-double amount = 801.0;
+double amount = 801.35;
 
 System.out.println(new BigDecimal(amount).setScale(2, RoundingMode.CEILING).doubleValue());
 DecimalFormat df = new DecimalFormat("#.00"); 
 System.out.println(df.format(amount));
 
+System.out.println(new BigDecimal(Double.toString(amount)).setScale(2));
+System.out.println(new BigDecimal(Double.toString(amount)).setScale(2).doubleValue());
+System.out.println(new BigDecimal(df.format(amount)).setScale(2));
+System.out.println(Double.parseDouble(df.format(801.00)));
+System.out.println(Double.valueOf(df.format(801.0)));
 }
 
 
